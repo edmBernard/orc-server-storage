3ab5490b47cbae71e9203334bad2d249d574787c
Erwan BERNARD <bernard.erwan@gmail.com>
2019-01-09 12:32:43 +0100

    add client using same notation as stryke

---
+++ include/stryke_client/client.hpp
@@ -0,0 +1,124 @@
+//
+//  Stryke
+//
+//  https://github.com/edmBernard/Stryke
+//
+//  Created by Erwan BERNARD on 02/12/2018.
+//
+//  Copyright (c) 2018 Erwan BERNARD. All rights reserved.
+//  Distributed under the Apache License, Version 2.0. (See accompanying
+//  file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+//
+#pragma once
+#ifndef STRYKE_CLIENT_HPP_
+#define STRYKE_CLIENT_HPP_
+
+
+#include "stryke/core.hpp"
+#include "nlohmann/json.hpp"
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <tuple>
+
+
+namespace stryke_client {
+
+namespace utils {
+// ==============================================================
+//  Create Cast Implementation: function to Cast tuple in json
+// ==============================================================
+
+template <typename Types, uint64_t N>
+bool addData(nlohmann::json &dst_data, const Types &data, std::string column_name) {
+  dst_data[column_name] = std::get<N>(data);
+  return true;
+}
+
+template <typename... Types, std::size_t... Indices>
+auto filljsonImpl(std::index_sequence<Indices...>, nlohmann::json &dst_data, std::tuple<Types...> &data, std::array<std::string, sizeof...(Types)> column_names) -> std::vector<bool> {
+  return {addData<std::tuple<Types...>, Indices>(dst_data, data, column_names[Indices])...};
+}
+
+template <typename... Types>
+auto filljson(nlohmann::json &dst_data, std::tuple<Types...> &data, std::array<std::string, sizeof...(Types)> column_names) {
+  return filljsonImpl<Types...>(std::index_sequence_for<Types...>(), dst_data, data, column_names);
+}
+
+} // namespace utils
+
+// ==============================================================
+//  Simple Writer Implementation
+// ==============================================================
+
+template <typename... Types>
+class OrcClient {
+public:
+  OrcClient(std::array<std::string, sizeof...(Types)> column_names, std::string host, std::string route, const stryke::WriterOptions &options)
+      : writeroptions(options), column_names(column_names), url(host + route) {
+  }
+
+  ~OrcClient() {
+    addToFile();
+  }
+
+  void write(Types... dataT) {
+    this->write_tuple(std::make_tuple(dataT...));
+  }
+
+  void close() {
+    this->addToFile();
+  }
+
+  void write_tuple(std::tuple<Types...> dataT) {
+
+    if (this->numValues >= this->writeroptions.batchSize) {
+      addToFile();
+    }
+
+    nlohmann::json elem;
+    utils::filljson(elem, dataT, this->column_names);
+    this->data.push_back(elem);
+
+    ++this->numValues;
+  }
+
+  void addToFile() {
+    static int count = 0;
+    auto r = cpr::Post(cpr::Url{this->url},
+                       cpr::Body{this->data.dump()},
+                       cpr::Header{{"Content-Type", "text/plain"}});
+
+    if (r.status_code != 200) {
+      std::cout << "response : " << r.status_code << " : " << r.text << std::endl;
+      std::cout << "message : " << this->data.dump() << std::endl;
+    }
+    ++count;
+    std::cout << "count : " << count << std::endl;
+    this->data.clear();
+
+    this->numValues = 0;
+  }
+
+private:
+  stryke::WriterOptions writeroptions;
+
+  nlohmann::json data = nlohmann::json::array(); // buffer that holds a batch of rows in json
+  std::array<std::string, sizeof...(Types)> column_names;
+
+  std::string url;
+
+  uint64_t numValues = 0; // num of lines read in a batch
+};
+
+} // namespace stryke_client
+
+#endif // !STRYKE_HPP_
---
+++ src/client_thread.cpp
@@ -0,0 +1,98 @@
+//
+//  stryke-server
+//
+//  https://github.com/edmBernard/stryke-server
+//
+//  Created by Erwan BERNARD on 07/01/2019.
+//
+//  Copyright (c) 2019 Erwan BERNARD. All rights reserved.
+//  Distributed under the Apache License, Version 2.0. (See accompanying
+//  file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+//
+
+#include <cpr/cpr.h>
+#include <curl/curl.h>
+#include "stryke/core.hpp"
+#include "stryke/thread.hpp"
+#include "stryke_client/client.hpp"
+#include <cxxopts.hpp>
+#include <iostream>
+#include <istream>
+#include <nlohmann/json.hpp>
+#include <ostream>
+#include <stdlib.h>
+#include <string>
+#include <chrono>
+#include <iomanip>
+
+using json = nlohmann::json;
+
+int main(int argc, char *argv[]) try {
+
+  // =================================================================================================
+  // Parse command line options
+  cxxopts::Options options(argv[0], "A client stryke server");
+  options.positional_help("[optional args]").show_positional_help();
+
+  // clang-format off
+    options.add_options()
+      ("help", "Print help")
+      ("h, host", "Server full hostname with port if needed", cxxopts::value<std::string>()->default_value("http://localhost:80"), "HOST")
+      ("b, batchsize", "Size of batch send", cxxopts::value<int>()->default_value("1000"), "BATCHSIZE")
+      ("n, number", "Number of point send", cxxopts::value<int>()->default_value("10000"), "NUMBER")
+      ("col0", "Columns name", cxxopts::value<std::string>()->default_value({"timestamp"}))
+      ("col1", "Columns name", cxxopts::value<std::string>()->default_value({"col1"}))
+      ("col2", "Columns name", cxxopts::value<std::string>()->default_value({"col2"}))
+      ("col3", "Columns name", cxxopts::value<std::string>()->default_value({"col3"}))
+      ("col4", "Columns name", cxxopts::value<std::string>()->default_value({"col4"}));
+  // clang-format on
+  options.parse_positional({"col0", "col1", "col2", "col3", "col4"});
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    std::cout << options.help({""}) << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  // Get Port Parameter
+  std::cout << "host :" << result["host"].as<std::string>() << std::endl;
+
+  std::cout << "col0 :" << result["col0"].as<std::string>() << std::endl;
+  std::cout << "col1 :" << result["col1"].as<std::string>() << std::endl;
+  std::cout << "col2 :" << result["col2"].as<std::string>() << std::endl;
+  std::cout << "col3 :" << result["col3"].as<std::string>() << std::endl;
+  std::cout << "col4 :" << result["col4"].as<std::string>() << std::endl;
+
+  auto start1 = std::chrono::high_resolution_clock::now();
+
+  stryke::WriterOptions writeroptions;
+  writeroptions.set_batch_size(result["batchsize"].as<int>());
+
+  stryke::OrcWriterThread<stryke_client::OrcClient, double, long, long, long, long> writer(
+    {
+      result["col0"].as<std::string>(),
+      result["col1"].as<std::string>(),
+      result["col2"].as<std::string>(),
+      result["col3"].as<std::string>(),
+      result["col4"].as<std::string>()
+    },
+    result["host"].as<std::string>(), std::string(""), writeroptions);
+
+  for (int j = 0; j < result["number"].as<int>(); ++j) {
+
+    double timestamp = std::chrono::high_resolution_clock::now().time_since_epoch().count() / 1000000000.;
+    writer.write(timestamp, j, j, j + 3,j + 4);
+
+  }
+
+  std::chrono::duration<double, std::milli> elapsed1 = std::chrono::high_resolution_clock::now() - start1;
+  std::cout << std::setw(40) << std::left << "processing time : before sync : " << elapsed1.count() << " ms\n";
+  writer.close_sync();
+  std::chrono::duration<double, std::milli> elapsed2 = std::chrono::high_resolution_clock::now() - start1;
+  std::cout << std::setw(40) << std::left << "processing time : after forced sync : " << elapsed2.count() << " ms\n";
+  return 0;
+
+} catch (const cxxopts::OptionException &e) {
+  std::cout << "Error parsing options: " << e.what() << std::endl;
+  return EXIT_FAILURE;
+}
